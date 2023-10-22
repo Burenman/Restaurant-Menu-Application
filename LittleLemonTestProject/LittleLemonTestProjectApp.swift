c1e99491de32ded503a0cda3841b78ae2b3cff25
Viktor <Burenin777@gmail.com>
2023-10-22 16:26:11 -0400

    Initial Commit

---
+++ LittleLemonTestProject/LittleLemonTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonTestProjectApp.swift
+//  LittleLemonTestProject
+//
+//  Created by Viktor on 2023-10-22.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
